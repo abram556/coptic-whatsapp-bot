064fa9e999a1b47d7362600350e82d119e272426
abram556 <abraammerzik56@gmail.com>
2026-05-05 04:40:18 +0300

    Add settings.js

---
+++ settings.js
@@ -0,0 +1,24 @@
+const settings = {
+  packname:    'مركز اللغة القبطية',
+  author:      'إبرام مرزق',
+  botName:     'مركز اللغة القبطية',
+  botOwner:    'إبرام مرزق',
+  ownerNumber: '201114884405',
+  adminSecret: '44',
+  version:     '7.0.0',
+  mode:        'PUBLIC',
+  maxStoreMessages:   20,
+  storeWriteInterval: 10000,
+  autoReplies: {
+    'hi':      'أهلاً! 👋 اكتب 1 للتعلم، 2 للقاموس، 3 للنتيجة.',
+    'hello':   'مرحباً! 😊 اكتب 1 للتعلم، 2 للقاموس، 3 للنتيجة.',
+    'مرحبا':   'أهلاً وسهلاً! 👋',
+    'السلام':  'وعليكم السلام ورحمة الله! 🤝',
+    'شكرا':    'العفو! 😊',
+    'owner':   'اضغط /owner للتواصل مع المالك 👑'
+  },
+  PREFIX: '/',
+  BROADCAST_DELAY_MS: 3600000
+};
+
+module.exports = settings;
